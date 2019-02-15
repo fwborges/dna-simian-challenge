a80b3241d93a099dc20671750228c9df8b3f46c0
Felipe Borges <felipe.santos@payulatam.com>
2019-02-15 19:10:05 -0200

    Move validator to use case

--- src/main/java/com/br/ml/challenge/simian/simianservice/validator/ValidateDnaChain.java
+++ src/main/java/com/br/ml/challenge/simian/simianservice/validator/ValidateDnaChain.java
@@ -6,19 +6,7 @@ public class ValidateDnaChain {
 
     public void execute(String[] dnaChain) {
 
-        int length = dnaChain.length;
 
-        Arrays.stream(dnaChain).forEach(dna -> {
-
-                    if(dna.length() != length) {
-                        throw new RuntimeException("Matriz não é quadrada");
-                    }
-
-                    if(!dna.matches("//([ACGT])//g")) {
-                        throw new RuntimeException("Matriz contem valores não permitidos");
-                    }
-
-                });
 
 
     }
